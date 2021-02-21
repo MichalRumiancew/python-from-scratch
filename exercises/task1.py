d9ff805f21fbfe2457a0e60f3ff78dd9faa8ffb3
MichalRumiancew <michalr11@gmail.com>
2021-02-21 16:38:14 +0100

    task1

---
+++ exercises/task1.py
@@ -0,0 +1,37 @@
+
+# Zad. 1 Wykorzystując poznane funkcje:
+
+# zwiększ pierwszą literę w podanej zmiennej:
+#      imie = "adam"
+
+# pomniejsz litery w podanej zmiennej:
+
+#  imie = "GenOWefa"
+
+
+# powiększ wszystkie litery w podanej zmiennej:
+   
+#      imie = "Anastazja"
+
+
+import math
+
+imie = "adam"
+
+imie = imie.capitalize()
+
+imie = "Arkadiusz"
+imiePoZmianie = imie.lower()
+
+imie = "GenOWefa"
+imiemalelitery = imie.lower()
+
+imie = "Anastazja"
+imieduze = imie.upper
+
+
+print(imiePoZmianie)
+print(imiemalelitery)
+print(imieduze)
+
+print(imie)
