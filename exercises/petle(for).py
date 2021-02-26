cc54492e3265f3d545d3a47d9e4769e68af5a857
MichalRumiancew <michalr11@gmail.com>
2021-02-26 20:21:40 +0100

    a

---
+++ exercises/petle(for).py
@@ -0,0 +1,19 @@
+wynik = 0
+
+"""
+i = 0
+while i < 4:
+    x = int(input("Podaj liczbe: "))
+    wynik += x
+    i += 1
+
+print("Wynik dodawania liczb to:", wynik)
+"""
+
+
+for i in range(1200):
+    if (i % 2 == 0):
+        print("Liczba", i, " jest parzysta")
+ 
+
+print("Wynik dodawania liczb to:", wynik)
